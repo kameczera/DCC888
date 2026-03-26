25ae3b9855f647065cd3afa055e61bca61adcd95
kamei <leokamei1@gmail.com>
2026-03-26 15:50:29 -0300

    exercise parsing done

--- Parsing/driver.py
+++ Parsing/driver.py
@@ -6,5 +6,7 @@ from lang import interp
 if __name__ == "__main__":
     lines = sys.stdin.readlines()
     env, program = todo.file2cfg_and_env(lines)
+    # for inst in program:
+    #     print(inst)
     final_env = interp(program[0], env)
     final_env.dump()
